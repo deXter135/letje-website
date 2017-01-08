fcd3576a3752237ea6f252928cd13e2479d29915
Martijn Dekker <m.h.dekker@hotmail.com>
2017-01-08 22:08:11 +0100

    index photo is af

--- js/init.js
+++ js/init.js
@@ -6,3 +6,12 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 250) {
+    $('#hideBeforeScroll').fadeIn();
+  } else {
+    $('#hideBeforeScroll').fadeOut();
+  }
+});
